21393919e8b6d9e58cbb928c81e8b62ec23e3ca7
Sandy Yu <throughthegrapevines@gmail.com>
2014-08-17 11:21:48 -0400

    add quantity count to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,15 @@ class Cart < ActiveRecord::Base
   # existence of line items is dependent on the existence of the cart, 
   # any dependents will be destroyed if cart is deleted
   has_many :line_items, dependent: :destroy
+
+  def add_product(product_id)
+    # checks whether list of items include the product we're adding 
+    current_item = line_items.find_by(product_id: product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
 end
